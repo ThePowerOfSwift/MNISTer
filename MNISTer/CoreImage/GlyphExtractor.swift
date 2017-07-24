b5c56ab12a1ec39d08b00e0ea8b337076367855d
Will McGinty <mcgintw@gmail.com>
2017-07-23 22:41:09 -0500

    Create struct for extracting glyphs

---
+++ MNISTer/CoreImage/GlyphExtractor.swift
@@ -0,0 +1,18 @@
+//
+//  GlyphExtractor.swift
+//  MNISTer
+//
+//  Created by William McGinty on 7/23/17.
+//  Copyright © 2017 William McGinty. All rights reserved.
+//
+
+import UIKit
+import CoreImage
+
+struct GlyphExtractor {
+    
+    static func glyphImages(containedIn image: UIImage) -> [UIImage] {
+        
+    }
+    
+}
